4e0d7c4eb3c80933845f2e5b5196470399351872
timotej-orcic <timotej.orcic@gmail.com>
2018-01-23 10:48:53 +0100

    Ubacivanje YOLO algoritma i njegove podrske na projekat

--- clockReckognition/test.py
+++ clockReckognition/test.py
@@ -4,19 +4,19 @@ import numpy as np
 
 img = cv2.imread('images/nesa.jpg', 0)
 
-img = cv2.medianBlur(img,5)
-cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+img = cv2.medianBlur(img, 5)
+cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 height, width, channels = cimg.shape
 
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-                            param1=300,param2=50,minRadius=0,maxRadius=0)
+circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20,
+                           param1=300, param2=50, minRadius=0, maxRadius=0)
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
+circles = np.int16(np.around(circles))
+for i in circles[0, :]:
     # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    cv2.circle(cimg, (i[0], i[1]), i[2], (0,255,0), 2)
     # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
 
 blackImg = np.zeros((height,width,channels), np.uint8)
 
@@ -30,7 +30,7 @@ for circle in circles[0,:]:
                 blackImg[i,j] = img[i,j]
 
 edges = cv2.Canny(blackImg,100,200)
-minLineLength = 30
+minLineLength = 50
 maxLineGap = 10
 lines = cv2.HoughLinesP(edges,1,np.pi/180,5, minLineLength, maxLineGap)
 for x1,y1,x2,y2 in lines[0]:
